40b59016cf35e9a1172b87a8b2472d099ef7307e
DeploymentsPractice <deploymentspractice@gmail.com>
2022-03-07 13:01:25 +0530

    or gate commit

--- or.py
+++ or.py
@@ -1,23 +1,26 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data, save_model, save_plot
+from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 
-OR = {
-    "x1": [0,0,1,1],
-    "x2": [0,1,0,1],
-    "y": [0,1,1,1],
-}
 
-df = pd.DataFrame(OR)
-X, y = prepare_data(df)
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(data)
+    print(df)
+    X, y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+    _ = model.total_loss()
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
 
-ETA = 0.2  # 0 and 1
-EPOCHS = 15
 
-model = Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(X, y)
-
-_ = model.total_loss()
-
-save_model(model,"or.model")
-save_plot(df,"or.png",model)
+if __name__ == '__main__':
+    OR = {
+        "x1": [0, 0, 1, 1],
+        "x2": [0, 1, 0, 1],
+        "y": [0, 1, 1, 1],
+    }
+    ETA = 0.3  # 0 and 1
+    EPOCHS = 10
+    main(data=OR, modelName="or.model",
+         plotName="or.png", eta=ETA, epochs=EPOCHS)
